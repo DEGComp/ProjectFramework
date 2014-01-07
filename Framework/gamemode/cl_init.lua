a0e54ea2d0fb63e5b6422ba7cef1fd012530285a
Serconker <serconker@felthatters.net>
2014-01-07 22:47:55 +0000

    The position is key.

--- Framework/gamemode/cl_init.lua
+++ Framework/gamemode/cl_init.lua
@@ -34,19 +34,19 @@ function fwMenuDerma(ply)
 	end
 
 	chars:SetParent(base)
-	chars:SetText("Character selection")
+	chars:SetText("")
 	chars:SetSize(150, 20)
-	chars:SetPos(100, 150)
+	chars:SetPos(170, 160)
 	chars.DoClick = function()
-		chat.AddText(Color(40, 40, 60), "Char menu will now appear, maybe?")
+		chat.AddText(Color(100, 60, 40), "Char menu will now appear, maybe?")
 		base:Close()
 		fwCharmenu(ply)
 	end
 
 	directory:SetParent(base)
-	directory:SetText("Directory")
-	directory:SetSize(150, 20)
-	directory:SetPos(100, 175)
+	directory:SetText("")
+	directory:SetSize(90, 30)
+	directory:SetPos(200, 250)
 	directory.DoClick = function()
 		fwDirectory()
 	end
@@ -60,7 +60,7 @@ function fwMenuDerma(ply)
 	end
 	
 	function directory:Paint(w, h)
---		draw.RoundedBox(0, 0, 0, w, h, Color(255, 255, 255, 120))
+		draw.SimpleText("Directory", "Trebuchet24", 0, 0, Color(100, 60, 40, 255))
 	end
 	function chars.Paint(w, h)
 		draw.SimpleText("Character menu", "Trebuchet24", 0, 0, Color(100, 60, 40, 255))
@@ -71,46 +71,3 @@ function fwMenuDerma(ply)
 	end
 end
 usermessage.Hook("fw_menu_open", fwMenuDerma)
-/*
- /)
-		local dbase = vgui.Create("DFrame")
-		local inim = vgui.Create("DImage", dbase)
-		local info = vgui.Create("DButton",  dbase)
-		local close = vgui.Create("DButton",  dbase)
-		dbase:SetPos(ScrW() / 2.65, 0)
-		dbase:SetSize(ScrW() / 2.65, ScrH() / 2 + ScrH() / 4 + 140)
-		dbase:SetVisible(true)
-		dbase:SetTitle("")
-		dbase:SetDraggable(true)
-		dbase:ShowCloseButton(false)
-		dbase:MakePopup()
-
-		info:SetPos(200, 155)
-		info:SetFont("default")
-		info:SetText("")
-		info:SetSize(150, 50)
-		info.DoClick = function()
-			dbase:Close()
-			fwDirectory(1)
-		end
-		
-		close:SetPos(80, 593)
-		close:SetFont("default")
-		close:SetText("")
-		close:SetSize(75, 100)
-		close.DoClick = function()
-			dbase:Close()
-		end
-		
-		function info.Paint(w, h)
-			draw.SimpleText("Infomation", "Trebuchet24", 0, 0, Color(100, 60, 40, 255))
-		end
-		
-		function dbase:Paint(w, h)
-			draw.RoundedBox(0, 0, 0, w, h, Color(20, 20, 20, 0))
-		end
-		
-		function close:Paint(w, h)
-			draw.RoundedBox(0, 0, 0, w, h, Color(20, 20, 20, 60))
-		end
-*/
