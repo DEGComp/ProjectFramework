dbd01c0f8d60e939cddc86b59aed197140bc7152
Frenjo <Moore_H2@sky.com>
2014-01-07 18:36:16 +0000

    Updated shared.lua

--- Framework/gamemode/shared.lua
+++ Framework/gamemode/shared.lua
@@ -3,7 +3,7 @@ DeriveGamemode("sandbox")
 GM.Name = "Project Framework"
 GM.Author = "Frenjo | Serconker | Riekelt"
 GM.Email = ""
-GM.Website = "http://deencryptiongaming.org/"
+GM.Website = "http://deencryptiongaming.org/ & http://serconker.felthatters.net/"
 
 function GM:Initialize()
 	print("Framework shared initializing...")
