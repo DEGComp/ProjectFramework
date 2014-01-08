f9ba72f9aea090aa6d53e4e46e59fbd4372b6853
Serconker <serconker@felthatters.net>
2014-01-08 23:33:06 +0000

    That needs corrections too.

--- Framework/gamemode/init.lua
+++ Framework/gamemode/init.lua
@@ -1,13 +1,13 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
-AddCSLuaFile("framework/framework/cl_kernel.lua")
-AddCSLuaFile("framework/framework/sh_kernel.lua")
+AddCSLuaFile("framework/framework/cl_kernal.lua")
+AddCSLuaFile("framework/framework/sh_kernal.lua")
 AddCSLuaFile("framework/framework/derma/cl_character.lua")
 AddCSLuaFile("framework/framework/derma/cl_directory.lua")
 
 include("shared.lua")
-include("framework/framework/sv_kernel.lua")
-include("framework/framework/sh_kernel.lua")
+include("framework/framework/sv_kernal.lua")
+include("framework/framework/sh_kernal.lua")
 
 function GM:Initialize()
 	print("Framework kernel serverside initializing...")
