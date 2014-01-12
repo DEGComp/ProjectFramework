e1aa43227f54528d8c4e28c02456fa7535075906
LilSumac <msearch091@gmail.com>
2014-01-12 17:06:35 -0500

    Added whitelist table
    Add your SteamIDs.

--- Framework/garrysmod/gamemodes/framework/config/sv_config.lua
+++ Framework/garrysmod/gamemodes/framework/config/sv_config.lua
@@ -11,3 +11,11 @@ DATABASE_NAME = "fw_data";
 DATABASE_PORT = 3306;
 
 WHITELIST_ENABLED = false;
+
+//Add SteamID's here, make sure to add commas
+//after each entry, save for the last one!
+WHITELISTS = {
+  "zip",
+  "zam",
+  "STEAM_0:1:26293888"
+}
