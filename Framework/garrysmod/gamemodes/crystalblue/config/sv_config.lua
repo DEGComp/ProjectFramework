c74ee96f559b6b66b8ced15b403eab7f098e722e
Michael Search <msearch091@gmail.com>
2014-01-14 18:47:16 -0500

    Fix 0.005

--- Framework/garrysmod/gamemodes/crystalblue/config/sv_config.lua
+++ Framework/garrysmod/gamemodes/crystalblue/config/sv_config.lua
@@ -7,10 +7,10 @@
 local CCore = CCore;
 CCore.Config = {};
 
-CCore.Config.DATABASE_HOST = "localhost";
-CCore.Config.DATABASE_USER = "root";
-CCore.Config.DATABASE_PASS = "";
-CCore.Config.DATABASE_NAME = "fw_data";
+CCore.Config.DATABASE_HOST = "mysql.fpsplayers.com";
+CCore.Config.DATABASE_USER = "1085_JakeM";
+CCore.Config.DATABASE_PASS = " ronas1313";
+CCore.Config.DATABASE_NAME = "1085_project";
 CCore.Config.DATABASE_PORT = 3306;
 
 CCore.Config.WHITELIST_ENABLED = false;
